2cbf986735bec898a5726602cb4e8fedf5a10b84
Larry Chen <odragonbestna@gmail.com>
2024-09-04 23:58:17 -0400

    Improve CSS

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Social from "./Social";
 
 export default function Title() {
   return (
@@ -9,6 +10,20 @@ export default function Title() {
         an aspiring software developer studying Computer Science at the
         University of Waterloo.
       </h3>
+      <section className="icons-container">
+        <Social
+          link="https://www.instagram.com/l.arrychen/"
+          fa="fa-brands fa-instagram"
+        />
+        <Social
+          link="https://github.com/oDragon"
+          fa="fa-brands fa-github"
+        />
+        <Social
+          link="https://www.linkedin.com/in/-larrychen/"
+          fa="fa-brands fa-linkedin"
+        />
+      </section>
     </div>
   );
 }
