d4265045f41c849b1ddf1f5dadb752b42d0b7140
Larry Chen <odragonbestna@gmail.com>
2024-09-08 01:44:35 -0400

    add support for mobile devices

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -9,6 +9,20 @@ export default function Navigation() {
       <a href="#experience" className="nav-link">Experience</a>
       <a href="#projects" className="nav-link">Projects</a>
       <a href="#contact" className="nav-link">Contact</a>
+      <input type="checkbox" id="sidebar-active" />
+      <label id="overlay" for="sidebar-active"></label>
+      <label className="menu-icon" for="sidebar-active">
+        <i className="fa-solid fa-bars"></i>
+      </label>
+      <div className="links-container">
+        <label className="close-menu-icon" for="sidebar-active">
+          <i className="fa-solid fa-x"></i>
+        </label>
+        <a href="#about" className="nav-link-mobile">About</a>
+        <a href="#experience" className="nav-link-mobile">Experience</a>
+        <a href="#projects" className="nav-link-mobile">Projects</a>
+        <a href="#contact" className="nav-link-mobile">Contact</a>
+      </div>
     </nav>
   );
 }
